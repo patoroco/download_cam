be876b4617b4e8d1cde213c6765a14386babfb21
Jorge Maroto <patoroco@gmail.com>
2023-01-05 12:57:18 +0100

    duplicates

--- cleaning_directories.py
+++ cleaning_directories.py
@@ -1,23 +1,46 @@
 #!/usr/bin/env python
 
+import hashlib
 import os 
 import re
 import shutil
 
+
 IMAGES_FOLDER = "./imgs"
 
 for path in os.listdir(IMAGES_FOLDER):
-    if path.endswith(".jpg"):
-        pattern = r'(\d{4})-(\d{2})-(\d{2})'
-        m = re.search(pattern, path)
-        year = m.group(1)
-        month = m.group(2)
-        day = m.group(3)
+    if not path.endswith(".jpg"):
+        continue
 
-        new_directory = os.path.join(IMAGES_FOLDER, f"{year}/{month}/{day}")
-        os.makedirs(new_directory, exist_ok=True)
-        old_path = os.path.join(IMAGES_FOLDER, path)
-        new_path = os.path.join(new_directory, path)
-        print(f"{old_path} --> {new_path}")
+    pattern = r'(\d{4})-(\d{2})-(\d{2})'
+    m = re.search(pattern, path)
+    year = m.group(1)
+    month = m.group(2)
+    day = m.group(3)
 
-        shutil.move(old_path, new_path)
+    new_directory = os.path.join(IMAGES_FOLDER, f"{year}/{month}/{day}")
+    os.makedirs(new_directory, exist_ok=True)
+    old_path = os.path.join(IMAGES_FOLDER, path)
+    new_path = os.path.join(new_directory, path)
+    print(f"{old_path} --> {new_path}")
+
+    shutil.move(old_path, new_path)
+
+MERGING_PATHS = "./imgs/2023/01/05"
+hashes = {}
+
+for path in os.listdir(MERGING_PATHS):
+    if not path.endswith(".jpg"):
+        continue
+
+    file_path = os.path.join(MERGING_PATHS, path)
+    with open(file_path, "rb") as f:
+        data = f.read()
+        md5hash = hashlib.md5(data).hexdigest()
+
+        if hashes.get(md5hash):
+            print(f"{file_path} --> EXISTE!")
+
+        hashes[md5hash] = file_path
+        
+        print(f"{file_path} --> {md5hash}")
